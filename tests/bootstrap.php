aaf9a6577e4f1484b6adc53800e1ee8e5887283e
Akihito Koriyama <akihito.koriyama@gmail.com>
2015-12-03 17:56:52 +0900

    init db

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,13 +6,37 @@ use Doctrine\Common\Annotations\AnnotationRegistry;
 
 error_reporting(E_ALL);
 
-$loader = require dirname(__DIR__) . '/vendor/autoload.php';
-AnnotationRegistry::registerLoader([$loader, 'loadClass']);
+load: {
+    $loader = require dirname(__DIR__) . '/vendor/autoload.php';
+    AnnotationRegistry::registerLoader([$loader, 'loadClass']);
+}
 
-// set the application path into the globals so we can access it in the tests.
-$_ENV['TEST_DIR'] = __DIR__;
-$_ENV['TMP_DIR'] = __DIR__ . '/tmp';
+constants: {
+    // set the application path into the globals so we can access it in the tests.
+    $_ENV['APP_DIR'] = dirname(__DIR__);
+    $_ENV['TEST_DIR'] = __DIR__;
+    $_ENV['TMP_DIR'] = __DIR__ . '/tmp';
+    // set the resource client
+    $app = (new Bootstrap)->getApp('MyVendor\MyApi', 'app');
+    $GLOBALS['RESOURCE'] = $app->resource;
+}
 
-// set the resource client
-$app = (new Bootstrap)->getApp('MyVendor\MyApi', 'app');
-$GLOBALS['RESOURCE'] = $app->resource;
+database: {
+    try {
+        $pdo = new \PDO($GLOBALS['DB_DSN'], $GLOBALS['DB_USER']);
+        $pdo->exec("CREATE DATABASE IF NOT EXISTS {$GLOBALS['DB_DBNAME']}; use {$GLOBALS['DB_DBNAME']}");
+        $pdo->exec('SET SESSION query_cache_limit=4194304;');
+        $pdo->exec("SET @@GLOBAL.sql_mode='NO_ENGINE_SUBSTITUTION'");
+    } catch (PDOException $e) {
+        echo "Database connection failed: user:{$GLOBALS['DB_USER']} passwd:{$GLOBALS['DB_PASSWD']}" . PHP_EOL;
+        exit(1);
+    }
+    register_shutdown_function(function () use ($pdo) {
+        $pdo->exec("DROP DATABASE {$GLOBALS['DB_DBNAME']};");
+    });
+    // crate table
+    $sql = file_get_contents($_ENV['APP_DIR'] . '/var/db/scheme.sql');
+    $pdo->exec($sql);
+    // disconnect
+    $pdo = null;
+}
